3be06a997d55fe462da4bd2d59edbfef283db02a
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-03 11:57:53 +0200

    first commit in

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+    public static void main(String[] args) {
+            //comentario
+            String greetings = "hola bkbgkjb";
+            int num1 = 5;
+            int num2 = 3;
+            System.out.println("-------");
+            System.out.println(multiplicar(num1, num2));
+
+    }
+
+    public static int multiplicar (int x, int y) {
+        return (x*y);
+    }
+}

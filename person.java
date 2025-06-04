1c9476d3d62463620b95e9366ea45ced1989a474
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-04 11:47:38 +0200

    new java

---
+++ person.java
@@ -0,0 +1,15 @@
+public class person {
+    public static void main(String[] args){
+        byte edad = 22;
+        float altura = 1.78f;
+        double weight = 58.234234;
+        String name = "Pepe";
+
+        System.out.println(name + " que tiene " + edad + "y una altura de " + altura + "con peso de" + weight);
+
+    }
+
+    
+    // Comentario de una linea
+    /* Comentario multilinea */
+}

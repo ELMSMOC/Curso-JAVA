2cf3b0a861975031ba0024d14cfd1ee748691be6
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-05 10:27:36 +0200

    printF exercise

--- Dia 4; out/Out.java
+++ Dia 4; out/Out.java
@@ -1,10 +1,12 @@
 public class Out {
+    
     public static void main(String[] args){
 
         String name = "Euge";
+        String surname = "Lazaro";
+        int age = 33;
 
-
-        System.out.println("Hola " + name + ".\n Como estas?");
-        System.out.printf("Hola %s,\n Como estas? ", name);
+        System.out.println("Hola " + name + " " + surname + ".\n Como estas a tus " + age + " años?");
+        System.out.printf("Hola %s %s,\n Como estas a tus %i años? ", name, surname, age);
     }
 }

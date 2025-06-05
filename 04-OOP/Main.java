7561d1a60ad610c736d2ffd0719c4bdacba86428
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-05 12:01:03 +0200

    output format

--- 04-OOP/Main.java
+++ 04-OOP/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(string[] args){
-        
+        Character character1;
+
+        character1 = new Character;
+
+        System.out.println(character1);
     }
 }

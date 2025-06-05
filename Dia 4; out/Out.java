da611448ba60da1f5192b995a1eabd0ea96accd9
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-05 10:11:50 +0200

    Print f

---
+++ Dia 4; out/Out.java
@@ -0,0 +1,9 @@
+public class Out {
+    public static void main(String[] args){
+        String name = "Euge";
+
+
+        System.out.println("Hola " + name + ". Como estas?");
+        System.out.printf("Hola.., Como estas? ")
+    }
+}

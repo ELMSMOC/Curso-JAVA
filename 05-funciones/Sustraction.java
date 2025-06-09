172b28b7081bb785b82ea6201190cc584119eec4
Euge <euge@gmail.com>
2025-06-09 19:34:55 +0200

    sustraction fixed

--- 05-funciones/Sustraction.java
+++ 05-funciones/Sustraction.java
@@ -1,7 +1,7 @@
 public class Sustraction {
 
 
-    static int Sustraction(int num1, int num2){
+    static int sustraction (int num1, int num2){
         int result = num1 - num2;
         return result;
         // return num1 - num2;
@@ -10,7 +10,8 @@ public class Sustraction {
     static void showResult(int result){
         System.out.println("El resultado es " + result);
     }
+
     public static void main(String[] args){
-        showResult(Sustraction(30_400, 28_333));
+        showResult(sustraction(30_400, 28_333));
     }
 }
--- 05-funciones/funcionesSum.java
+++ 05-funciones/funcionesSum.java
@@ -1,4 +1,4 @@
-public class funciones {
+public class funcionesSum {
 
     /* Funcion: codigo ejecutable
       */

914f825e1ea2623e3bddfba1a393981958e3a323
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-10 10:17:20 +0200

    Funcion de area

--- 07-Shapes/Shapes.java
+++ 07-Shapes/Shapes.java
@@ -1,10 +1,13 @@
-public static Shapes {
+public class Shapes {
     
-    Static int AreaResult(int side) {
-        System.out.println(side*side);
+    public static double square(double side) {
+        double area = side * side;
+        return area;
     }
 
-    public static void main(Strings[] args){
-        AreaResult(5);
+    public static void main(String[] args){
+        System.out.println(square(2));
+        System.out.println(square(3.6));
+        System.out.println(square(5));
     }
 }

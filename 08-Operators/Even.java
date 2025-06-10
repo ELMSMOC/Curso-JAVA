adb9a83231b73a8ef25d177a8c55d7efa58cdb50
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-10 12:59:43 +0200

    even and odd practice

--- 08-Operators/Even.java
+++ 08-Operators/Even.java
@@ -1,18 +1,24 @@
 public class Even {
-    public static void main(String[] args){
 
-        
-
+    public static void mode1 {
         if (args.length == 0){
             System.out.println("Dime tu edad en la consola de comando");
         } else {
             int input = Integer.valueOf(args[0]);
             if (input % 2 == 0){
-                System.out.println("es par");
+                System.out.printf("El numero %s es par", input);
             } else {
-                System.out.println("es impar");
+                System.out.printf("El numero %s es impar", input);
             }
         }
+    }
 
+     public static void mode2 {
+
+    }
+    public static void main(String[] args){
+
+        mode1(args);
+        mode1(args);
     }
 }

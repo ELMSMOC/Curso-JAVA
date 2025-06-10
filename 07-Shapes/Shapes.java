ed74b56e731d45ce995b5b9f81df462427956d2c
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-10 09:39:40 +0200

    sdgsdg

---
+++ 07-Shapes/Shapes.java
@@ -0,0 +1,10 @@
+public static Shapes {
+    
+    Static int AreaResult(int side) {
+        System.out.println(side*side);
+    }
+
+    public static void main(Strings[] args){
+        AreaResult(5);
+    }
+}

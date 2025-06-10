52f1f3f02d6ccbd2c11e51d6d192c01909bd84b2
Euge <euge@gmail.com>
2025-06-10 19:39:15 +0200

    even odd

--- 08-Operators/Even.java
+++ 08-Operators/Even.java
@@ -1,6 +1,6 @@
 public class Even {
 
-    public static void mode1 {
+    public static void mode1(String[] args) {
         if (args.length == 0){
             System.out.println("Dime tu edad en la consola de comando");
         } else {
@@ -13,12 +13,50 @@ public class Even {
         }
     }
 
-     public static void mode2 {
-
+    public static void mode2(String[] args) { 
+        if (args.length == 0) {
+            System.out.println("Tienes que añadir un entero en la línea de comandos");
+            return;
+        }      
+        int number = Integer.valueOf(args[0]);
+        if( (number % 2) == 0) {
+            System.out.printf("El número %s es par", number);
+        } else {
+            System.out.printf("El número %s es impar", number);
+        }
     }
-    public static void main(String[] args){
 
-        mode1(args);
+    public static void mode3(String[] args) {
+        
+        if (args.length == 0) {
+            System.out.println("Tienes que añadir un entero en la línea de comandos");
+            return;
+        }      
+
+        int number = Integer.valueOf(args[0]);
+        String result = "impar";
+        if( (number % 2) == 0) {
+            result = "par";
+         } 
+        System.out.printf("El número %s es %s", number, result);
+     
+    }
+
+    public static void mode4(String[] args) {
+        
+        if (args.length == 0) {
+            System.out.println("Tienes que añadir un entero en la línea de comandos");
+            return;
+        }      
+
+        int number = Integer.valueOf(args[0]);
+        // (condición) ? valor : valor
+        String result = (number % 2) == 0 ?  "par" : "impar";
+        System.out.printf("El número %s es %s", number, result);
+     
+    }
+
+    public static void main(String[] args){
         mode1(args);
     }
 }

b1bbea2bd561053329079e82ca3a034e706935cb
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-10 10:30:21 +0200

    Sobrecarga de funciones

--- 07-Shapes/Shapes.java
+++ 07-Shapes/Shapes.java
@@ -1,13 +1,21 @@
 public class Shapes {
     
-    public static double square(double side) {
-        double area = side * side;
-        return area;
+
+    // Sobrecarga de funciones, se pueden añadir cuantas quieras con el mismo nombre mientras tengan tipos o parametros diferentes
+    
+    public static double area(double side) {
+        return side * side;
+    }
+
+    public static double area(double base, double height) {
+        return base * height;
     }
 
     public static void main(String[] args){
-        System.out.println(square(2));
-        System.out.println(square(3.6));
-        System.out.println(square(5));
+        System.out.println(area(2));
+        System.out.println(area(3.6));
+        System.out.println(area(5));
+
+        System.out.println(area(5, 2));
     }
 }

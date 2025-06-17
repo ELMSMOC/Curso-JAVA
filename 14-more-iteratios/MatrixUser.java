035ccae2fbd39dae7342417a5be1d05f0bda1d29
Euge <euge@gmail.com>
2025-06-17 20:30:31 +0200

    Matrices

--- 14-more-iteratios/MatrixUser.java
+++ 14-more-iteratios/MatrixUser.java
@@ -1,60 +1,115 @@
+// Le pedimos al usuario 9 animales y los guardamos como matriz de 3x3
+// Le presentamos los nombres que nos ha dado.  
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class MatrixUser {
 
-    static String[][] getData (){
-        String[][] animalMatrix = new  String [3][3];
+    static String[][] getData() {
+        String[][] animals = new String[3][3]; 
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < animalMatrix.length; i++) {
-            for (int j = 0; j < animalMatrix[0].length; j++) {
-                System.out.println("Dime un animal");
-                animalMatrix[i][j] = scanner.nextLine();
-            }
+        for (int i = 0; i < animals.length; i++) {
+            for (int j = 0; j < animals[0].length; j++) {
+                System.out.printf("Dime el animal %s\n", j+1);
+                animals[i][j] = scanner.nextLine();
+            }     
         }
-        
-        scanner.close();
-        return animalMatrix;
+        return animals;
     }
 
-    static void showData (String[][] data){
+    static void showInfo(String[][] data){
+        System.out.println("");
+        System.out.println("Aqui tienes tus animales");
         for (String[] row : data) {
-            for (String text : row) {
-                System.out.println(text);
+            for (String animal : row) {
+                System.out.println(animal);
             }
         }
     }
 
     // Misma funcion duplicada con distinto tipo de parametro
     static void showData (String[] data){
-        for (String[] row : data) {
-            for (String text : row) {
-                System.out.println(text);
-            }
+        for (String text : data) {
+            System.out.println(text);
         }
     }
 
-    static  void getAnimals(){
+    static String[][] getAnimals(){
         String[][] animals = new  String [3][3];
+        String[] animalKind = {
+            "Domestico",
+            "Salvaje",
+            "Reptil"
+        };
 
         String[] messages = {
             "Dime 3 domesticos",
             "Dime 3 salvajes",
             "Dime 3 no mamiferos"
         };
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(messages[i]);
+            
+            for (int j = 0; j < animals[0].length; j++) {
+                System.out.printf("Dime el animal %s %s\n", animalKind[i], j+1);
+                animals[i][j]= scanner.nextLine();
+            }
+            if (i < animals.length - 1){
+                System.out.println("Siguiente fila");
+            } else {
+                System.out.println("Gracias por todo");
+            }
+        }
+
+        return animals;
     }
 
-    static String[] selectRamdoms(string[][] data){
+    static String[] selectRandoms(String[][] data){
         String[] selectedAnimals = new String[data.length];
-        Ramdom r = new Ramdom();
+        Random r = new Random();
         for (int i = 0; i < selectedAnimals.length; i++) {
-             selectedAnimals[i] = data[i]
+             selectedAnimals[i] = data[i][r.nextInt(2)];
         }
+
+        return selectedAnimals;
     }
     
     public static void main(String[] args) {
-        String[][] animals = getAnimals();
         
-        showData(animals);
-        String[] selectedAnimals = selectRamdoms(animals);
+        // showInfo(getData());
+        //  showInfo(getAnimals());
+
+
+        String[][] animals = getAnimals();
+        showInfo(animals);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean wantContinue = true;
+
+        while(wantContinue) {
+
+            String[] selectAnimals = selectRandoms(animals);
+            showData(selectAnimals);
+
+
+            System.out.println("¿Quieres continuar (S/N)?");
+            String answer = scanner.nextLine();
+            if(
+                answer.toLowerCase().equals("n") ||
+                answer.toLowerCase().equals("no")
+            ) {
+                wantContinue = false;
+            }
+
+        }
+
+        scanner.close();
+        System.out.println("Gracias por todo");
+
+        
     }
 }

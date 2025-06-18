deba238c378d74465b6df88467714c448e985d51
Euge <euge@gmail.com>
2025-06-18 17:54:17 +0200

    copy teacher solution papel piedra tijera

--- 15-/factorial.java
+++ 15-/factorial.java
@@ -1,9 +1,19 @@
 public class factorial {
-    static void calcFactorial(){
-        
+    static int calcFactorial(int num){
+        if (num < 0){
+            num *= -1;
+        }
+
+        int result = 1;
+
+        for (int i = num; i > 0; i--) {
+            result *= i;
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
-        
+        System.out.println(calcFactorial(4));
     }
 }

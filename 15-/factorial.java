7d920cf69eb863350e48f828fb72d2637f40cf6d
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-06-19 09:34:31 +0200

    funciones recursivas

--- 15-/factorial.java
+++ 15-/factorial.java
@@ -1,19 +1,28 @@
 public class factorial {
-    static int calcFactorial(int num){
+    static long calcFactorial(long num){
         if (num < 0){
             num *= -1;
         }
 
-        int result = 1;
+        long result = 1;
 
-        for (int i = num; i > 0; i--) {
+        for (long i = num; i > 0; i--) {
             result *= i;
         }
 
         return result;
     }
 
+    static long recursiveFactorial(long number){
+        if (number == 1){
+            return number;
+        }
+        long result = number * calcFactorial(number - 1);
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(calcFactorial(4));
+        System.out.println(calcFactorial(20));
+       System.out.println(recursiveFactorial(5));
     }
 }

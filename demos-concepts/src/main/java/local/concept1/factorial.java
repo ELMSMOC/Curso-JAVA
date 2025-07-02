7aa9543e8069e33ddfe33c51ec4763aec658f585
Euge <euge@gmail.com>
2025-07-02 23:53:32 +0200

    asd

--- demos-concepts/src/main/java/local/concept1/factorial.java
+++ demos-concepts/src/main/java/local/concept1/factorial.java
@@ -1,23 +1,44 @@
 package local.concept1;
 
 import java.text.NumberFormat;
+import java.util.Locale;
+
+import local.exceptions.BusinessException;
+import local.exceptions.ErrorCodes;
+import local.exceptions.TechnicalException;
 
 public class Factorial {
+
+    static void showNumber(long number) {
+        NumberFormat nf = NumberFormat.getInstance(Locale.GERMANY);
+        System.out.println(nf.format(number));
+    }
+
     private short num;
+
+    Factorial() {
+    }
 
     Factorial(short num){
         this.num = num;
     }
 
-    long calcFactorial(){
-        return calcFactorial();
+    long calcFactorial() throws BusinessException{
+        return calcFactorial(num);
     }
 
-    long calcFactorial(long num){
-        if (num < 0){
-            throw new IndexOutOfBoundsException();
+    long calcFactorial(short num) throws BusinessException{
+        if (num < 0) {
+           String message = "No se puede calcular el factorial de " + num; 
+           // throw new TechnicalException(message);
+           throw new BusinessException(ErrorCodes.ERROR_NEGATIVE, message);
         }
 
+        if (num > 10) {
+            String message = "No se puede calcular el factorial de " + num; 
+           // throw new TechnicalException(message);
+           throw new BusinessException(ErrorCodes.ERROR_BIGGER_20, message);
+        }
         long result = 1;
 
         for (long i = num; i > 0; i--) {
@@ -37,10 +58,15 @@ public class Factorial {
         Factorial fact = new Factorial();
         short num = 10;
         num = -5;
-        try {
+        num = 22;
 
-        } catch (Exception e){}
+        try {
+            long result = fact.calcFactorial(num);
+            showNumber(result);
+        } catch (Exception e){
+            System.err.println(e.getMessage());
+        }
         
-        System.out.println(fact.calcFactorial(num));
+        System.out.println("Fin");
     }
 }
--- demos-concepts/src/main/java/local/exceptions/ErrorCodes.java
+++ demos-concepts/src/main/java/local/exceptions/ErrorCodes.java
@@ -1,6 +1,7 @@
 package local.exceptions;
 
 public enum ErrorCodes {
+    ERROR_BIGGER_20("No puede ser mayor de 20"),
     ERROR_EVEN("No se admiten pares"),
     ERROR_NEGATIVE("No se admiten negativos"),
     ERROR_ZERO("No se puede dividir por 0");

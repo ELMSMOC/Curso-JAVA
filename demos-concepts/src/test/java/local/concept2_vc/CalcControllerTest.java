5e68f51991a0711127484da3548fc04aa0618c2c
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-07-04 13:08:06 +0200

    test

--- demos-concepts/src/test/java/local/concept2_vc/CalcControllerTest.java
+++ demos-concepts/src/test/java/local/concept2_vc/CalcControllerTest.java
@@ -3,6 +3,9 @@ package local.concept2_vc;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import local.exceptions.BusinessException;
+import local.exceptions.ErrorCodes;
+
 public class CalcControllerTest {
     @Test
     public void testAddOk() {
@@ -38,5 +41,21 @@ public class CalcControllerTest {
         int result = calculator.add();
         assertEquals("Suma 5 y 3", 9, result);
 
+    }
+
+    @Test
+    public void testDivide(){
+        int num1 = 0;
+        int num2 = 0;
+        CalcController cc = new CalcController();
+        cc.setNum1(num1);
+        cc.setNum2(num2);
+        int result;
+        try {
+            result = cc.intDivide();
+        } catch (BusinessException e) {
+            BusinessException expected = new BusinessException(ErrorCodes.ERROR_ZERO, null);
+            assertEquals(expected , e);
+        }
     }
 }

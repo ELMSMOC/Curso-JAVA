396acae7891211ac7795e154cfdf89897f68c016
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-07-10 13:18:04 +0200

    coleections

---
+++ demos-concepts/src/main/java/local/collections/CollectionDemo.java
@@ -0,0 +1,13 @@
+package local.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionDemo {
+
+    static Collection coll = new ArrayList();
+
+    public static void main(String[] args) {
+        
+    }
+}

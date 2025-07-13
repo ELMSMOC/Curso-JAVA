d5189ea8574689b2695e43346f25bdbfc4d8a621
Euge <euge@gmail.com>
2025-07-13 22:24:36 +0200

    sgs

---
+++ demos-concepts/src/main/java/local/collections/exercises/Ex05_Cards/Card.java
@@ -0,0 +1,11 @@
+package local.collections.exercises.Ex05_Cards;
+
+public class Card {
+    private String cardName;
+    private Suit suit;
+
+    public Card(String cardName, Suit suit) {
+        this.cardName = cardName;
+        this.suit = suit;
+    }
+}
---
+++ demos-concepts/src/main/java/local/collections/exercises/Ex05_Cards/Suit.java
@@ -0,0 +1,9 @@
+package local.collections.exercises.Ex05_Cards;
+
+public enum Suit {
+    HEARTS ("Corazones"),
+    SPADES("Picas"),
+    DIAMONDS("Diamantes"),
+    CLUBS("Tréboles");
+
+}

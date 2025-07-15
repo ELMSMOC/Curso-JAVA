ea6e58347f2d7bebd1444d43d681c439b5c0b366
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-07-15 13:19:36 +0200

    file persistency

--- demos-persis/src/main/java/com/cdsb/files/FileSystem1.java
+++ demos-persis/src/main/java/com/cdsb/files/FileSystem1.java
@@ -1,26 +1,23 @@
 package com.cdsb.files;
 
 import java.io.File;
-import java.util.Arrays;
 
 public class FileSystem1 {
 
     static void listFiles(String pathname){
         File file = new File(pathname);
-        String[] list = file.list();
+        File[] list = file.listFiles();
 
-        for (String item : list) {
-            char type = getType(pathname + "/" + item);
-            System.out.println(type + " " + item);
+        for (File fileOrFolder : list) {
+            char type = fileOrFolder.isDirectory() ? 'D' : 'F';
+            System.out.println(type + " " + fileOrFolder.getName());
         }
     }
 
-    static char getType(String pathname){
-        File file = new File(pathname);
-        return file.isDirectory() ? 'D': 'F';
-    }
 
     public static void main(String[] args) {
         String pathname = "demos-persis";
+        pathname = "patata";
+        listFiles(pathname);
     }
 }

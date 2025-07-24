fdd71b672f0bcbcd9747c1015aba8e7f9897a975
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-07-24 13:21:07 +0200

    asdfg

--- TEST/General/src/Libreria/Library.java
+++ TEST/General/src/Libreria/Library.java
@@ -1,22 +1,22 @@
-// package Libreria;
+package Libreria;
 
-// import Libreria.Book;
+import Libreria.Book;
 
-// import java.util.List;
+import java.util.List;
 
-// public class Library implements ILibrary {
-//     List<Book> bookList;
+public class Library implements ILibrary {
+    List<Book> bookList;
 
-//     public void addBook (Book book){
-//         bookList.add(book);
-//     }
+    public void addBook (Book book){
+        bookList.add(book);
+    }
 
-//     public void showBookList(String word){
-//         List<Book> listFiltered = new List<Book>();
+    public void showBookList(String word){
+        List<Book> listFiltered = new List<Book>();
 
-//         for (java.awt.print.Book book : bookList) {
-//             System.out.println(book.toString());
-//         }
-//     }
+        for (java.awt.print.Book book : bookList) {
+            System.out.println(book.toString());
+        }
+    }
 
-// }
+}

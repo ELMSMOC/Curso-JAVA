c1b4f779de081e9b96056d4aa3a4a549affbf367
Eugenio Lazaro <eugeniolazaro@gmail.com>
2025-07-24 12:53:21 +0200

    asdfg

--- TEST/General/src/Libreria/Library.java
+++ TEST/General/src/Libreria/Library.java
@@ -7,11 +7,26 @@ import java.util.List;
 public class Library implements ILibrary {
     List<Book> bookList;
 
-    void addBook (Book book){
+    public void addBook (Book book){
         bookList.add(book);
     };
 
+    void showBookList(String word){
+        List<Book> listFiltered = new List<Book>();
+
+        for (Book book : bookList) {
+            System.out.println(book);
+        }
+    };
+
     public static void main(String[] args) {
-        
+        Library lb = new Library();
+        lb.addBook(new Book("Moby Dick", "Jules Verne", 1870, false));
+        lb.addBook(new Book("Los tigres de Mompracem", "Emilio Salgari", 1900, false));
+        lb.addBook(new Book("Papillon", "Henri Charriere", 1969, false));
+        lb.addBook(new Book("Rebeldes", "Susan E. Hinton", 1967, false));
+        lb.addBook(new Book("El instituto", "Stephen King", 2019, false));
+
+        lb.showBookList("papillon");
     }
 }
